4efd6f507548052ff860b8bb794442b1205ad734
Tansy Arron <tansy.arron@gmail.com>
2015-02-12 00:23:58 -0500

    updated

--- connectWiFi.ino
+++ connectWiFi.ino
@@ -1,22 +1,17 @@
 #include <SPI.h>
 #include <WiFi.h>
 
-const char ssid[] = " ";    // Your SSID
-const char pass[] = " ";    // Your Network Password
-
-int status = WL_IDLE_STATUS;      // the Wifi radio's status
-IPAddress server(10,0,0,2);       // Your Server IP 
-
-WiFiClient client;
-int AnalogPin = 0;
-int val = 0;
-
 void setup() {
   // connect to wifi
   connectNetwork();
 }
 
 void loop() {
+  IPAddress server(10,0,0,2);       // Your Server IP 
+  WiFiClient client;
+  int AnalogPin = 0;
+  int val = 0;
+
   // check for connection to server and start sending data.
   if (client.connect(server, 6880)) {
     val = analogRead(A0);
@@ -26,10 +21,11 @@ void loop() {
 }
 
 void connectNetwork() {
+  const char ssid[] = " ";    // Your SSID
+  const char pass[] = " ";    // Your Network Password
+  int status = WL_IDLE_STATUS;      // the Wifi radio's status
   // attempt to connect using WPA2 encryption:
-  status = WiFi.begin(ssid, pass);
-  // if you're not connected, try again:
-  if ( status != WL_CONNECTED) { 
-    connectNetwork();
+  while (status != WL_CONNECTED) {
+    status = WiFi.begin(ssid, pass)
   }
 }
